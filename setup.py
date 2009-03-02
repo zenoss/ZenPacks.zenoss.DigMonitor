b4b08fc5a3afb4d110a1ceb7904df9554033da69
cluther <cluther@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2009-03-02 13:40:16 +0000

    * Fixes #4561: Core ZenPack changes to support restricted ZenManager access.
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core/zenpacks/ZenPacks.zenoss.DigMonitor@25767 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@
 # These variables are overwritten by Zenoss when the ZenPack is exported
 # or saved.  Do not modify them directly here.
 NAME = 'ZenPacks.zenoss.DigMonitor'
-VERSION = '1.0'
+VERSION = '1.0.1'
 AUTHOR = 'Zenoss'
 LICENSE = ''
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.zenoss']
 PACKAGES = ['ZenPacks', 'ZenPacks.zenoss', 'ZenPacks.zenoss.DigMonitor']
 INSTALL_REQUIRES = []
-COMPAT_ZENOSS_VERS = '>= 2.1'
+COMPAT_ZENOSS_VERS = '>= 2.3.70'
 PREV_ZENPACK_NAME = ''
 # STOP_REPLACEMENTS
 ################################
